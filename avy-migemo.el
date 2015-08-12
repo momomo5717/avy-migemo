7103cb0ef4f1ae67f31ef399d13426d48a79099b
momomo5717 <momomo5717@users.noreply.github.com>
2015-08-13 00:15:04 +0900

    Bump version number

--- avy-migemo.el
+++ avy-migemo.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2015 momomo5717
 
 ;; Keywords: avy, migemo
-;; Version: 0.1.0
+;; Version: 0.1.1
 ;; Package-Requires:((emacs "24.4") (avy "0.3") (migemo "1.9"))
 ;; URL: https://github.com/momomo5717/avy-migemo
 
