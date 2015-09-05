2641470e65970bfbef33a915a072ae352b005025
momomo5717 <momomo5717@users.noreply.github.com>
2015-09-06 00:42:22 +0900

    Add avy-migemo--overlay-at, avy-migemo--overlay-at-full

--- avy-migemo.el
+++ avy-migemo.el
@@ -23,27 +23,37 @@
 
 ;;; Commentary:
 
-;; This package provides the following functions:
+;; This package is a minor mode of avy for using migemo.
+;;
+;; The following functions are provided:
 ;;
 ;;   + avy-migemo-goto-char
-;;   + avy-migemo-goto-char-in-line
 ;;   + avy-migemo-goto-char-2
-;;   + avy-migemo-isearch
-;;   + avy-migemo-goto-word-1
+;;   + avy-migemo-goto-char-in-line
 ;;   + avy-migemo-goto-char-timer
+;;   + avy-migemo-goto-word-1
+;;   + avy-migemo-isearch
+;;   + avy-migemo--overlay-at
+;;   + avy-migemo--overlay-at-full
 ;;
 ;;  These are the same as avy's predefined functions
 ;;  except for adding candidates via migemo (simply using migemo as `regexp-quote').
 ;;
+;; Further information is available from:
+;; https://github.com/momomo5717/avy-migemo  (README.org)
+
 ;; Setup:
 ;; (add-to-list 'load-path "/path/to/avy-migemo")
 ;; (require 'avy-migemo)
 ;; ;; If you override avy's predefined functions using `advice-add',
 ;; (avy-migemo-mode 1)
+;;
+;; ;; You can toggle `avy-migemo-mode' by M-x avy-migemo-mode.
 
 ;;; Code:
 (require 'avy)
 (require 'migemo)
+(require 'cl-lib)
 
 (defgroup avy-migemo nil
   "avy with migemo."
@@ -57,11 +67,13 @@ It takes a string and returns a regular expression."
 
 (defcustom avy-migemo-function-names
   '(avy-migemo-goto-char
-    avy-migemo-goto-char-in-line
     avy-migemo-goto-char-2
-    avy-migemo-isearch
+    avy-migemo-goto-char-in-line
+    avy-migemo-goto-char-timer
     avy-migemo-goto-word-1
-    avy-migemo-goto-char-timer)
+    avy-migemo-isearch
+    avy-migemo--overlay-at
+    avy-migemo--overlay-at-full)
   "Function names for overriding avy's functions."
   :set (lambda (symbol value)
          (if (and (boundp symbol) (boundp 'avy-migemo-mode))
@@ -72,6 +84,162 @@ It takes a string and returns a regular expression."
            (set symbol value))))
 
 ;;;###autoload
+;;;###autoload
+(define-minor-mode avy-migemo-mode
+  "Override avy's functions."
+  :global t
+  (dolist (name avy-migemo-function-names)
+    (let ((predefined-name
+           (intern (replace-regexp-in-string
+                    "-migemo" "" (symbol-name name)))))
+      (if avy-migemo-mode
+          (advice-add predefined-name :override name)
+        (advice-remove predefined-name name)))))
+
+
+;; avy functions for migemo
+
+(defun avy-migemo--rest-old-str (old-str+ str)
+  "Retrun a new string which is a part of  OLD-STR+.
+STR is compared with string width of OLD-STR+."
+  (cl-loop
+   with old-ls = (string-to-list old-str+)
+   with new-ls = (string-to-list str)
+   with pre-width  = 0
+   with char-count = 0
+   until (null new-ls) do
+   (cond
+    ((and (zerop char-count) (car old-ls))
+     (setq pre-width (max (char-width (pop old-ls)) 1))
+     (cl-incf char-count (max (char-width (pop new-ls)) 1)))
+    ((> pre-width char-count)
+     (cl-incf char-count (max (char-width (pop new-ls)) 1)))
+    ((null old-ls)
+     (setq new-ls nil pre-width 0 char-count 0))
+    (t (setq char-count 0)))
+   finally return
+   (apply #'string
+          (nconc (make-list (- pre-width char-count) ? ) old-ls))))
+
+(defun avy-migemo--overlay-at (path leaf)
+  "The same as `avy--overlay-at' except adapting it for migemo."
+  (let* ((path (mapcar #'avy--key-to-char path))
+         (str (propertize
+               (string (car (last path)))
+               'face 'avy-lead-face))
+         (pt (+ (if (consp (car leaf))
+                    (caar leaf)
+                  (car leaf))
+                avy--overlay-offset))
+         (wnd (cdr leaf))
+         (ol (make-overlay pt (1+ pt)
+                           (window-buffer wnd)))
+         (old-str (with-selected-window wnd
+                    (buffer-substring pt (1+ pt)))))
+    (when avy-background
+      (setq old-str (propertize
+                     old-str 'face 'avy-background-face)))
+    (overlay-put ol 'window wnd)
+    (overlay-put ol 'display
+                 (if (string= old-str "\n")
+                     (concat str "\n")
+                   ;; Adapt for migemo
+                   (let* ((old-str+ (with-selected-window wnd
+                                        (buffer-substring
+                                         pt (1+ pt)))))
+                     (if (= (max (string-width old-str+) 1) 1)
+                         str
+                       (concat str (avy-migemo--rest-old-str old-str+ str))))))
+    (push ol avy--overlays-lead)))
+
+(defun avy-migemo--overlay-at-full (path leaf)
+  "The same as `avy--overlay-at-full' except adapting it for migemo."
+  (let* ((path (mapcar #'avy--key-to-char path))
+         (str (propertize
+               (apply #'string (reverse path))
+               'face 'avy-lead-face))
+         (len (length path))
+         (beg (if (consp (car leaf))
+                  (caar leaf)
+                (car leaf)))
+         (wnd (cdr leaf))
+         oov)
+    (dotimes (i len)
+      (set-text-properties (- len i 1) (- len i)
+                           `(face ,(nth i avy-lead-faces))
+                           str))
+    (when (eq avy-style 'de-bruijn)
+      (setq str (concat
+                 (propertize avy-current-path
+                             'face 'avy-lead-face-1)
+                 str))
+      (setq len (length str)))
+    (with-selected-window wnd
+      (save-excursion
+        (goto-char beg)
+        (when (setq oov
+                    (delq nil
+                          (mapcar
+                           (lambda (o)
+                             (and (eq (overlay-get o 'category) 'avy)
+                                  (eq (overlay-get o 'window) wnd)
+                                  (overlay-start o)))
+                           (overlays-in (point) (min (+ (point) len)
+                                                     (line-end-position))))))
+          (setq len (- (apply #'min oov) beg))
+          (setq str (substring str 0 len)))
+        (let ((other-ov (cl-find-if
+                         (lambda (o)
+                           (and (eq (overlay-get o 'category) 'avy)
+                                (eq (overlay-start o) beg)
+                                (not (eq (overlay-get o 'window) wnd))))
+                         (overlays-in (point) (min (+ (point) len)
+                                                   (line-end-position))))))
+          (when (and other-ov
+                     (> (overlay-end other-ov)
+                        (+ beg len)))
+            (setq str (concat str (buffer-substring
+                                   (+ beg len)
+                                   (overlay-end other-ov))))
+            (setq len (- (overlay-end other-ov)
+                         beg))))
+        (let* ((end (if (= beg (line-end-position))
+                        (1+ beg)
+                      (min (+ beg
+                              (if (eq (char-after) ?\t)
+                                  1
+                                len))
+                           (line-end-position))))
+               (ol (make-overlay
+                    beg end
+                    (current-buffer)))
+               (old-str (buffer-substring beg (1+ beg))))
+          (when avy-background
+            (setq old-str (propertize
+                           old-str 'face 'avy-background-face)))
+          (overlay-put ol 'window wnd)
+          (overlay-put ol 'category 'avy)
+          (overlay-put ol 'display
+                       (cond ((string= old-str "\n")
+                              (concat str "\n"))
+                             ((string= old-str "\t")
+                              (concat str (make-string (- tab-width len) ?\ )))
+                             (t ;; Adapt for migemo
+                              (let* ((other-char-p
+                                      (cl-loop for c across str
+                                               for i from 0
+                                               unless (memq c avy-keys)
+                                               return i))
+                                     (str (if other-char-p
+                                          (substring str 0 other-char-p) str))
+                                     (new-width (string-width str))
+                                     (old-str+ (buffer-substring beg end)))
+                                (if (= (string-width old-str+) new-width)
+                                    str
+                                  (concat str (avy-migemo--rest-old-str old-str+ str)))))))
+          (push ol avy--overlays-lead))))))
+
+;;;###autoload
 (defun avy-migemo-goto-char (char &optional arg)
   "The same as `avy-migemo-goto-char' except for the candidates via migemo."
   (interactive (list (read-char "char: " t)
@@ -80,21 +248,10 @@ It takes a string and returns a regular expression."
     (avy--generic-jump
      (if (= 13 char)
          "\n"
+       ;; Adapt for migemo
        (funcall avy-migemo-get-function (string char)))
      arg
      avy-style)))
-
-;;;###autoload
-(defun avy-migemo-goto-char-in-line (char)
-  "The same as `avy-goto-char-in-line' except for the candidates via migemo."
-  (interactive (list (read-char "char: " t)))
-  (avy-with avy-goto-char
-    (avy--generic-jump
-     (funcall avy-migemo-get-function (string char))
-     avy-all-windows
-     avy-style
-     (line-beginning-position)
-     (line-end-position))))
 
 ;;;###autoload
 (defun avy-migemo-goto-char-2 (char1 char2 &optional arg)
@@ -104,22 +261,39 @@ It takes a string and returns a regular expression."
                      current-prefix-arg))
   (avy-with avy-goto-char-2
     (avy--generic-jump
+     ;; Adapt for migemo
      (funcall avy-migemo-get-function (string char1 char2))
      arg
      avy-style)))
 
 ;;;###autoload
-(defun avy-migemo-isearch ()
-  "The same as `avy-isearch' except for the candidates via migemo."
-  (interactive)
-  (avy-with avy-isearch
-    (let ((avy-background nil))
-      (avy--process
-       (avy--regex-candidates
-        (concat isearch-string "\\|"
-                (funcall avy-migemo-get-function isearch-string)))
-       (avy--style-fn avy-style))
-      (isearch-done))))
+(defun avy-migemo-goto-char-in-line (char)
+  "The same as `avy-goto-char-in-line' except for the candidates via migemo."
+  (interactive (list (read-char "char: " t)))
+  (avy-with avy-goto-char
+    (avy--generic-jump
+     ;; Adapt for migemo
+     (funcall avy-migemo-get-function (string char))
+     avy-all-windows
+     avy-style
+     (line-beginning-position)
+     (line-end-position))))
+
+;;;###autoload
+(defun avy-migemo-goto-char-timer (&optional arg)
+  "The same as `avy-goto-char-timer' except for the candidates via migemo."
+  (interactive "P")
+  (let ((c1 (read-char "char 1: " t))
+        (c2 (read-char "char 2: " t avy-timeout-seconds)))
+    (avy-with avy-goto-char-timer
+      (avy--generic-jump
+       ;; Adapt for migemo
+       (funcall avy-migemo-get-function
+                (if c2
+                    (string c1 c2)
+                  (string c1)))
+       arg
+       avy-style))))
 
 ;;;###autoload
 (defun avy-migemo-goto-word-1 (char &optional arg)
@@ -132,40 +306,27 @@ It takes a string and returns a regular expression."
                          "\\.")
                         ((and avy-word-punc-regexp
                               (string-match avy-word-punc-regexp str))
+                         ;; Adapt for migemo
                          (funcall avy-migemo-get-function str))
-                        (t
+                        (t ;; Adapt for migemo
                          (concat
                           "\\b"
                           (funcall avy-migemo-get-function str))))))
       (avy--generic-jump regex arg avy-style))))
 
 ;;;###autoload
-(defun avy-migemo-goto-char-timer (&optional arg)
-  "The same as `avy-goto-char-timer' except for the candidates via migemo."
-  (interactive "P")
-  (let ((c1 (read-char "char 1: " t))
-        (c2 (read-char "char 2: " t avy-timeout-seconds)))
-    (avy-with avy-goto-char-timer
-      (avy--generic-jump
-       (funcall avy-migemo-get-function
-                (if c2
-                    (string c1 c2)
-                  (string c1)))
-       arg
-       avy-style))))
-
-;;;###autoload
-(define-minor-mode avy-migemo-mode
-  "Override avy's functions."
-  :global t
-  :group 'avy
-  (dolist (name avy-migemo-function-names)
-    (let ((predefined-name
-           (intern (replace-regexp-in-string
-                    "-migemo" "" (symbol-name name)))))
-      (if avy-migemo-mode
-          (advice-add predefined-name :override name)
-        (advice-remove predefined-name name)))))
+(defun avy-migemo-isearch ()
+  "The same as `avy-isearch' except for the candidates via migemo."
+  (interactive)
+  (avy-with avy-isearch
+    (let ((avy-background nil))
+      (avy--process
+       (avy--regex-candidates
+        ;; Adapt for migemo
+        (concat isearch-string "\\|"
+                (funcall avy-migemo-get-function isearch-string)))
+       (avy--style-fn avy-style))
+      (isearch-done))))
 
 (provide 'avy-migemo)
 ;;; avy-migemo.el ends here
